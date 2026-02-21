8cf63ed211110202da0f57a97512c15423e9ff18
khoasdyn <khoasdyn@gmail.com>
2026-02-21 16:48:02 +0700

    Initial Commit

---
+++ RabbitHole/RabbitHoleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RabbitHoleApp.swift
+//  RabbitHole
+//
+//  Created by khoasdyn on 21/2/26.
+//
+
+import SwiftUI
+
+@main
+struct RabbitHoleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
